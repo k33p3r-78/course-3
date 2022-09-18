82e6acec3ad1289b9c76504a66859fdd44f7cc11
Игорь Демьяненко <cmp.int0x80@gmail.com>
2022-09-18 07:06:12 +0300

    indent align

--- lesson 6/lesson 6.sql
+++ lesson 6/lesson 6.sql
@@ -7,32 +7,32 @@ SELECT
     first_name,
     last_name,
     (
-		SELECT COUNT(*) 
-		FROM messages 
+        SELECT COUNT(*) 
+        FROM messages 
         WHERE 
-			-- Выбираем пользователей, которые отправляли сообщения заданному пользователю.
-			to_user_id = users.id 
+            -- Выбираем пользователей, которые отправляли сообщения заданному пользователю.
+            to_user_id = users.id 
             -- Проверяем статус дружбы.
             AND user_id IN (SELECT friend_id FROM friendships WHERE user_id = users.id AND friendships.status_id = 2)
-	) AS total_msgs
+    ) AS total_msgs
 FROM 
-	users
+    users
 ORDER BY
-	total_msgs DESC
+    total_msgs DESC
 LIMIT 1;
 
 -- #3
 SELECT SUM(likes_count) AS top10_likes_sum 
 FROM
-	(SELECT
-		(SELECT COUNT(*) FROM user_likes WHERE for_user_id = profiles.user_id) AS likes_count
-		FROM profiles
-		ORDER BY birthday DESC
-		LIMIT 10) AS pass;
+    (SELECT
+        (SELECT COUNT(*) FROM user_likes WHERE for_user_id = profiles.user_id) AS likes_count
+        FROM profiles
+        ORDER BY birthday DESC
+        LIMIT 10) AS pass;
 
 -- #4
 SELECT 
-	(SELECT gender FROM profiles WHERE user_id = user_likes.user_id) AS gender,
+    (SELECT gender FROM profiles WHERE user_id = user_likes.user_id) AS gender,
     COUNT(*) AS sum
 FROM user_likes
 GROUP BY gender
@@ -56,7 +56,7 @@ LIMIT 1;
 -- GROUP BY user_id
 -- ORDER BY likes_count;
 SELECT
-	first_name,
+    first_name,
     (SELECT COUNT(*) FROM user_likes WHERE user_likes.user_id = users.id) as act
 FROM users
 ORDER BY act
